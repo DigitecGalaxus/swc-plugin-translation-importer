9ea6388a0c7ecfa139f792b085cb190cd06553d9
Martin Disch <martindisch@gmail.com>
2024-01-29 13:59:01 +0100

    Migrate to external snapshots

---
+++ tests/__swc_snapshots__/src/lib.rs/icu_by_language.js
@@ -0,0 +1,10 @@
+import { __i18n_5dec8611ff95ef5ade67844d5a0b16a7c2020762363f0231d8bf4ba7aeca7474 } from "../../.cache/translations.i18n?dev";
+const foo = __icuByLanguage(
+  __i18n_5dec8611ff95ef5ade67844d5a0b16a7c2020762363f0231d8bf4ba7aeca7474 ||
+    "Pluralized items ordered",
+  language,
+  {
+    category,
+    stockCount,
+  }
+);
